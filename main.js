1ed046a49ab11c8bed0f81c9a550d0097717d521
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2014-01-21 11:23:03 +0100

    Directive basic behaviors

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-var app = angular.module('superhero', []);
+var app = angular.module('behaviorApp', []);
 
-app.directive('superman', function () {
-    return {
-        restrict: "M",
-        link: function () {
-            alert("I am working");
-        }
+app.directive('enter', function () {
+    return function ($scope, element) {
+        element.bind('mouseenter', function () {
+            console.log('inside!!');
+        });
+    };
+});
+
+app.directive('leave', function () {
+    return function ($scope, element) {
+        element.bind('mouseleave', function () {
+            console.log('leave!!');
+        });
     };
 });
