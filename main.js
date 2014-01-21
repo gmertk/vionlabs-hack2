99067b1c5f255bdb12bf5eaa5403294f121f3a1e
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2014-01-21 10:31:21 +0100

    Add a filter

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ myApp.factory('Data', function () {
     return {message: 'I am from service'};
 });
 
+myApp.filter('reverse', function(Data) {
+   return function (text) {
+       return text.split('').reverse().join('') + Data.message;
+   };
+});
+
 function FirstCtrl($scope, Data) {
     $scope.data = Data;
 }
 
 function SecondCtrl($scope, Data) {
     $scope.data = Data;
-
-    $scope.reverseMessage = function (message) {
-        return message.split('').reverse().join('');
-    };
 }
