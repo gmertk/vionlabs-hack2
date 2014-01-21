7d13376551937a6dbe53d4b972ed7674ca9daa79
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2014-01-21 10:46:21 +0100

    Add filters

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 var myApp = angular.module('myApp', []);
-myApp.factory('Data', function () {
-    return {message: 'I am from service'};
+myApp.factory('Avengers', function() {
+    var Avengers = {};
+    Avengers.cast = [
+        {
+            name: "Mert Karadogan",
+            character: "Maven"
+        },
+        {
+            name: "Orcun Doganci",
+            character: "Iron Man"
+        },
+        {
+            name: "Yigit Karadogan",
+            character: "Agent"
+        }
+    ];
+    return Avengers;
 });
 
-myApp.filter('reverse', function(Data) {
-   return function (text) {
-       return text.split('').reverse().join('') + Data.message;
-   };
-});
-
-function FirstCtrl($scope, Data) {
-    $scope.data = Data;
-}
-
-function SecondCtrl($scope, Data) {
-    $scope.data = Data;
+function AvengersCtrl($scope, Avengers) {
+    $scope.avengers = Avengers;
 }
