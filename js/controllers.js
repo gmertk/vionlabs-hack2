1db36864ab0129e9138202e5281c4486c85672c2
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2014-01-24 22:14:15 +0100

    Add simple home page

--- js/controllers.js
+++ js/controllers.js
@@ -2,8 +2,6 @@ angular.module('myApp.controllers', [])
   .controller('MyCtrl', ['$scope', "$firebase", "$firebaseSimpleLogin", function($scope, $firebase, $firebaseSimpleLogin) {
     var ref = new Firebase("https://mert-firebase.firebaseio.com/users");
     $scope.auth = $firebaseSimpleLogin(ref);
-    $scope.$watch('auth', function(newVal, oldVal) {
-      console.log(newVal);
-    });
+    
   }])
   ;
