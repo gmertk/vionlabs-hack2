a51d11c7c0580e024a2130682e91d90884bd7418
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2014-01-26 01:10:57 +0100

    Login button removed from top. Reverse order added. Getting tired

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 angular.module('app', ['firebase', 'ui.bootstrap','myApp.controllers', 'myApp.routes',
-    'waitForAuth', 'myApp.services', 'myApp.config', 'myApp.directives'])
+    'waitForAuth', 'myApp.services', 'myApp.config', 'myApp.directives', 'myApp.filters'])
     .run(['loginService', '$rootScope', 'FBURL', function(loginService, $rootScope, FBURL) {
         $rootScope.auth = loginService.init('/login');
         $rootScope.FBURL = FBURL;
